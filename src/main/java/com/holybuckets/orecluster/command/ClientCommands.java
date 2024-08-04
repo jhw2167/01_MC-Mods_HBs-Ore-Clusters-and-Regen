a476c1f88730aecf3d0b340d45a22cd68af8702c
jhw2167 <jhw2167@columbia.edu>
2024-08-04 18:57:22 -0500

    -adding client command handling classes -added new packages specific to event, core behavior and commands -Added new variable to detect player load accross the application

---
+++ src/main/java/com/holybuckets/orecluster/command/ClientCommands.java
@@ -0,0 +1,55 @@
+package com.holybuckets.orecluster.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+
+//Project imports
+
+public class ClientCommands {
+
+    private static final String PREFIX = "hbCmd";
+
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher)
+    {
+        dispatcher.register(Commands.literal(PREFIX)
+            .then(Commands.argument("command", StringArgumentType.string())
+                .executes(context -> execute(context, ""))
+            .then(Commands.argument("arg1", StringArgumentType.string())
+                .executes(context -> execute(context, ""))
+            .then(Commands.argument("arg2", StringArgumentType.string())
+                .executes(context -> execute(context, ""))
+            .then(Commands.argument("arg3", StringArgumentType.string())
+                .executes(context -> execute(context, ""))
+            .then(Commands.argument("arg4", StringArgumentType.string())
+                .executes(context -> execute(context, "")))))))
+                .executes(context -> execute(context, "")));
+
+
+    }
+
+    private static final int totalArgs = 4;
+    private static int execute(CommandContext<CommandSourceStack> context, String defaultArg ) throws CommandSyntaxException
+    {
+        String cmd = StringArgumentType.getString(context, "command");
+        String[] args = new String[totalArgs];
+        for( int i = 0; i < totalArgs; i++ )
+        {
+            args[i] = StringArgumentType.getString(context, "arg" + (i+1));
+            args[i] = (args[i]) == null ? defaultArg : args[i];
+            cmd += " " + args[i];
+        }
+
+        LocalPlayer player = Minecraft.getInstance().player;
+        if (player != null) {
+            player.sendSystemMessage(Component.literal("Received message: " + cmd));
+        }
+        return 1;
+    }
+}
